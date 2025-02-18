48c85e7d6eb9f98da45b88a97645216fd1ef92c1
Hamza862-Naeem <mhamzanaeem862@gmail.com>
2025-02-18 21:33:08 +0500

    first commit

---
+++ Intro.dart
@@ -0,0 +1,16 @@
+
+
+
+//Declare constant type of int set value 7.
+
+// void main() {
+//   const int myConstant = 7;
+//   print(myConstant);  // Output: 7
+// }
+
+//Write a program in Dart that finds simple interest. Formula= (p * t * r) / 100
+
+
+void main(){
+  
+}
